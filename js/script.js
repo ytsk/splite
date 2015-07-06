10ad9139fbc8ff651c4ce059621e600fd4a41778
yutosakurai <yt.a.skr@gmail.com>
2015-07-07 04:16:43 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+	$(function(){
+		$('.month_box').hide();
+	//
+		$('.year_title').click(function(e){
+			$('+.month_box',this).slideToggle(700);
+
+
+		});
+	});
+
+	
+	$(function(){
+	$('a[href^=#]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
